bab7b9ca828563ec7ae21db38232ab744b02dc53
Maddisen Mohnsen <memohnsen@gmail.com>
2025-08-29 14:55:49 -0400

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Maddisen Mohnsen on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
